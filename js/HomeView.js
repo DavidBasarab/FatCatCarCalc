0afa97281e1b7cc066693262f193e9891d261418
David Basarab <dbasarab617@gmail.com>
2013-04-02 17:17:07 -0400

    Playing with the initial style and the slider.

--- js/HomeView.js
+++ js/HomeView.js
@@ -4,8 +4,24 @@ var HomeView = function (theData) {
         this.el = $('<div />');
     };
 
+    this.setUpCarSlider = function() {
+        var slider = this.el.find('#carPrice-slider');
+        slider.slider({
+            max: 50000,
+            min: 0,
+            step: 250,
+            value: theData.carPrice,
+            change: function(e, ui) {
+                $('#carPrice').val(ui.value);
+            }
+        });
+    }
+
     this.render = function () {
         this.el.html(HomeView.template(theData));
+
+        this.setUpCarSlider();
+
         return this;
     };
 
