9550f8e80c536acf1ef987019f1078cf9afae0a3
David Basarab <dbasarab617@gmail.com>
2013-04-03 13:15:47 -0400

    Starting to get closer to what I want for the UI.  Colors are still really bad.

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,42 +1,14 @@
 var HomeView = function (theData) {
 
     this.initialize = function () {
-        this.el = $('<div />');
     };
 
-
-
-    this.setUpCarSlider = function () {
-        var slider = this.el.find('#carPrice-slider');
-        slider.slider({
-            max: 50000,
-            min: 0,
-            step: 250,
-            value: theData.carPrice,
-            stop: function (e, ui) {
-                $('#carPrice').val(ui.value);
-//                $('#carPrice').formatCurrency({
-//                    roundToDecimalPlace: 0
-//                });
-            }
-        });
-    }
-
     this.render = function () {
-        var movies = [
-            { name: "The Red Violin", releaseYear: "1998" },
-            { name: "Eyes Wide Shut", releaseYear: "1999" },
-            { name: "The Inheritance", releaseYear: "1976" }
-        ];
-        this.el.html($('#home-template').render(movies));
-
-        this.el.find('#test-idea').trigger('create');
-//        this.el.html(HomeView.template(theData));
-//        var $priceInput = this.el.find('#carPrice');
-//        $priceInput.formatCurrency({
-//            roundToDecimalPlace: 0
-//        });
-//        this.setUpCarSlider();
+        $('#carPrice-slider').val(theData.carPrice);
+        $('#carPrice-slider').slider('refresh')
+        $('#tradeIn-slider').val(theData.tradeInPrice);
+        $('#tradeIn-slider').slider('refresh');
+        $('#payoff-text').val(theData.tradeInAmount);
 
         return this;
     };
