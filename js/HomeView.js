53aa7564721b53ef2dc0f2b693f3747e4dc90866
David Basarab <dbasarab617@gmail.com>
2013-04-03 12:46:58 -0400

    Want to see if this new view looks like on the phone.

--- js/HomeView.js
+++ js/HomeView.js
@@ -4,14 +4,16 @@ var HomeView = function (theData) {
         this.el = $('<div />');
     };
 
-    this.setUpCarSlider = function() {
+
+
+    this.setUpCarSlider = function () {
         var slider = this.el.find('#carPrice-slider');
         slider.slider({
             max: 50000,
             min: 0,
             step: 250,
             value: theData.carPrice,
-            stop: function(e, ui) {
+            stop: function (e, ui) {
                 $('#carPrice').val(ui.value);
 //                $('#carPrice').formatCurrency({
 //                    roundToDecimalPlace: 0
@@ -21,7 +23,15 @@ var HomeView = function (theData) {
     }
 
     this.render = function () {
-        this.el.html(HomeView.template(theData));
+        var movies = [
+            { name: "The Red Violin", releaseYear: "1998" },
+            { name: "Eyes Wide Shut", releaseYear: "1999" },
+            { name: "The Inheritance", releaseYear: "1976" }
+        ];
+        this.el.html($('#home-template').render(movies));
+
+        this.el.find('#test-idea').trigger('create');
+//        this.el.html(HomeView.template(theData));
 //        var $priceInput = this.el.find('#carPrice');
 //        $priceInput.formatCurrency({
 //            roundToDecimalPlace: 0
@@ -34,4 +44,4 @@ var HomeView = function (theData) {
     this.initialize();
 }
 
-HomeView.template = Handlebars.compile($('#home-template').html());
+
